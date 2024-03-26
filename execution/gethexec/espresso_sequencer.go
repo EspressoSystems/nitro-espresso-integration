7cc6c69f5f464f1e99a7e1939616fdcc49a8bdd3
ImJeremyHe <yiliang.he@qq.com>
2024-03-26 16:34:28 +0800

    Remove the fetching hotshot block error logs

--- execution/gethexec/espresso_sequencer.go
+++ execution/gethexec/espresso_sequencer.go
@@ -73,12 +73,10 @@ func (s *EspressoSequencer) createBlock(ctx context.Context) (returnValue bool) 
 	nextSeqBlockNum := s.hotShotState.nextSeqBlockNum
 	header, err := s.hotShotState.client.FetchHeaderByHeight(ctx, nextSeqBlockNum)
 	if err != nil {
-		log.Warn("Unable to fetch header for block number, will retry", "block_num", nextSeqBlockNum)
 		return false
 	}
 	arbTxns, err := s.hotShotState.client.FetchTransactionsInBlock(ctx, header.Height, s.namespace)
 	if err != nil {
-		log.Error("Error fetching transactions", "err", err)
 		return false
 
 	}
