730d37f2e353f56abd8167d1195ef50ceda65841
Zach Showalter <zacshowa@gmail.com>
2024-11-04 15:30:17 -0500

    Make current lib.rs c abi compatible

--- arbitrator/espresso-crypto-helper/src/lib.rs
+++ arbitrator/espresso-crypto-helper/src/lib.rs
@@ -21,6 +21,7 @@ use sequencer_data_structures::{
     field_to_u256, BlockMerkleCommitment, BlockMerkleTree, Header, Transaction,
 };
 use sha2::{Digest, Sha256};
+use std::{ffi::CStr, os::raw::c_char};
 use tagged_base64::TaggedBase64;
 
 #[derive(
@@ -55,17 +56,31 @@ pub type Proof = Vec<MerkleNode<Commitment<Header>, u64, Sha3Node>>;
 pub type CircuitField = ark_ed_on_bn254::Fq;
 
 // Helper function to verify a block merkle proof.
-//
+// For the purposes of FFI with go, the parameters in to this function must be of type const* c_char
 // proof_bytes: Byte representation of a block merkle proof.
 // root_bytes: Byte representation of a Sha3Node merkle root.
 // header_bytes: Byte representation of the HotShot header being validated as a Merkle leaf.
 // circuit_block_bytes: Circuit representation of the HotShot header commitment returned by the light client contract.
-pub fn verify_merkle_proof_helper(
-    proof_bytes: &[u8],
-    header_bytes: &[u8],
-    block_comm_bytes: &[u8],
-    circuit_block_bytes: &[u8],
-) {
+#[no_mangle]
+pub extern "C" fn verify_merkle_proof_helper(
+    proof_c_bytes: *const c_char,
+    header_c_bytes: *const c_char,
+    block_comm_c_bytes: *const c_char,
+    circuit_block_c_bytes: *const c_char,
+) -> bool {
+    let proof_c_str = unsafe { CStr::from_ptr(proof_c_bytes) };
+
+    let header_c_str = unsafe { CStr::from_ptr(header_c_bytes) };
+
+    let block_comm_c_str = unsafe { CStr::from_ptr(block_comm_c_bytes) };
+
+    let circuit_block_c_str = unsafe { CStr::from_ptr(circuit_block_c_bytes) };
+
+    let proof_bytes = proof_c_str.to_bytes();
+    let header_bytes = header_c_str.to_bytes();
+    let block_comm_bytes = block_comm_c_str.to_bytes();
+    let circuit_block_bytes = circuit_block_c_str.to_bytes();
+
     let proof_str = std::str::from_utf8(proof_bytes).unwrap();
     let header_str = std::str::from_utf8(header_bytes).unwrap();
     let block_comm_str = std::str::from_utf8(block_comm_bytes).unwrap();
@@ -90,8 +105,13 @@ pub fn verify_merkle_proof_helper(
     let local_block_comm_u256 = field_to_u256(field_bytes);
     let circuit_block_comm_u256 = U256::from_little_endian(circuit_block_bytes);
 
-    assert!(proved_comm == header_comm);
-    assert!(local_block_comm_u256 == circuit_block_comm_u256);
+    //assert!(proved_comm == header_comm);
+    //assert!(local_block_comm_u256 == circuit_block_comm_u256);
+
+    if (proved_comm == header_comm) && (local_block_comm_u256 == circuit_block_comm_u256) {
+        return true;
+    }
+    return false;
 }
 
 // Helper function to verify a VID namespace proof that takes the byte representations of the proof,
@@ -101,14 +121,29 @@ pub fn verify_merkle_proof_helper(
 // commit_bytes: Byte representation of a TaggedBase64 payload commitment string.
 // ns_table_bytes: Raw bytes of the namespace table.
 // tx_comm_bytes: Byte representation of a hex encoded Sha256 digest that the transaction set commits to.
-pub fn verify_namespace_helper(
+#[no_mangle]
+pub extern "C" fn verify_namespace_helper(
     namespace: u64,
-    proof_bytes: &[u8],
-    commit_bytes: &[u8],
-    ns_table_bytes: &[u8],
-    tx_comm_bytes: &[u8],
-    common_data_bytes: &[u8],
-) {
+    proof_c_bytes: *const c_char,
+    commit_c_bytes: *const c_char,
+    ns_table_c_bytes: *const c_char,
+    tx_comm_c_bytes: *const c_char,
+    common_data_c_bytes: *const c_char,
+) -> bool {
+    let ns_table_c_str = unsafe { CStr::from_ptr(ns_table_c_bytes) };
+
+    let proof_c_str = unsafe { CStr::from_ptr(proof_c_bytes) };
+    let commit_c_str = unsafe { CStr::from_ptr(commit_c_bytes) };
+    let tx_comm_c_str = unsafe { CStr::from_ptr(tx_comm_c_bytes) };
+
+    let common_data_c_str = unsafe { CStr::from_ptr(common_data_c_bytes) };
+
+    let ns_table_bytes = ns_table_c_str.to_bytes();
+    let proof_bytes = proof_c_str.to_bytes();
+    let commit_bytes = commit_c_str.to_bytes();
+    let tx_comm_bytes = tx_comm_c_str.to_bytes();
+    let common_data_bytes = common_data_c_str.to_bytes();
+
     let proof_str = std::str::from_utf8(proof_bytes).unwrap();
     let commit_str = std::str::from_utf8(commit_bytes).unwrap();
     let txn_comm_str = std::str::from_utf8(tx_comm_bytes).unwrap();
@@ -127,8 +162,12 @@ pub fn verify_namespace_helper(
     let namespace: u32 = namespace.try_into().unwrap();
     let txns_comm = hash_txns(namespace, &txns);
 
-    assert!(ns == namespace.into());
-    assert!(txns_comm == txn_comm_str);
+    //assert!(ns == namespace.into());
+    //assert!(txns_comm == txn_comm_str);
+    if (ns == namespace.into()) && (txns_comm == txn_comm_str) {
+        return true;
+    }
+    return false;
 }
 
 // TODO: Use Commit trait: https://github.com/EspressoSystems/nitro-espresso-integration/issues/88
