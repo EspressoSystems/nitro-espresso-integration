acc8423c6aabe109cab0567a87aa17c7ca25d6fe
Zach Showalter <zacshowa@gmail.com>
2024-11-05 12:05:34 -0500

    update flake.nix to new go version and fix errors

--- flake.nix
+++ flake.nix
@@ -10,17 +10,16 @@
 
   outputs = { flake-utils, nixpkgs, foundry, rust-overlay, ... }:
     let
-      goVersion = 21; # Change this to update the whole stack
+      goVersion = 22; # Change this to update the whole stack
       overlays = [
         (import rust-overlay)
-        (final: prev: {
+        (final: prev: rec {
           go = prev."go_1_${toString goVersion}";
           # Overlaying nodejs here to ensure nodePackages use the desired
-          # version of nodejs. Arbitrum use nodejs v16 but that's EOL and has
-          # been removed from nixpkgs, v18 also works.
-          nodejs = prev.nodejs-18_x;
-          pnpm = prev.nodePackages.pnpm;
-          yarn = prev.nodePackages.yarn;
+          # version of nodejs. Offchainlabs suggests nodejs v18 in the docs.
+          nodejs = prev.nodejs_18;
+          yarn = (prev.yarn.override { inherit nodejs; });
+          pnpm = (prev.pnpm.override { inherit nodejs; });
         })
         foundry.overlay
       ];
