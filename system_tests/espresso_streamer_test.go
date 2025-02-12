ef1a8cd27ab6f38d9fe30dc3b49fa3f2fe7f7060
Sneh Koul <snehkoul1999@gmail.com>
2025-02-12 17:09:46 -0500

    add tests for espresso streamer

--- system_tests/espresso_streamer_test.go
+++ system_tests/espresso_streamer_test.go
@@ -1,61 +1,70 @@
 package arbtest
 
-// TODO: fix this test
+import (
+	"context"
+	"testing"
+	"time"
 
-// func createEspressoStreamer(builder *NodeBuilder) *arbstate.EspressoStreamer {
+	"github.com/offchainlabs/nitro/arbstate"
+	"github.com/offchainlabs/nitro/util/headerreader"
+)
 
-// 	namespace := builder.chainConfig.ChainID.Uint64()
-// 	hotshotUrl := "http://127.0.0.1:41000"
-// 	nextHotshotBlockNum := uint64(1)
-// 	return arbstate.NewEspressoStreamer(
-// 		namespace,
-// 		[]string{hotshotUrl, hotshotUrl},
-// 		nextHotshotBlockNum,
-// 		1*time.Second,
-// 		250*time.Millisecond,
-// 	)
-// }
+func createEspressoStreamer(builder *NodeBuilder) *arbstate.EspressoStreamer {
 
-// func TestEspressoStreamer(t *testing.T) {
+	namespace := builder.chainConfig.ChainID.Uint64()
+	hotshotUrl := "http://127.0.0.1:41000"
+	nextHotshotBlockNum := uint64(1)
+	return arbstate.NewEspressoStreamer(
+		namespace,
+		[]string{hotshotUrl, hotshotUrl},
+		nextHotshotBlockNum,
+		1*time.Second,
+		250*time.Millisecond,
+		builder.L1Info.GetAddress("Bridge"),
+		"http://0.0.0.0:8545",
+		headerreader.DefaultConfig,
+		builder.L1Info.GetAddress("EspressoTEEVerifierMock"),
+	)
+}
 
-// 	ctx, cancel := context.WithCancel(context.Background())
-// 	defer cancel()
+func TestEspressoStreamer(t *testing.T) {
 
-// 	builder, cleanup := createL1AndL2Node(ctx, t, true)
-// 	defer cleanup()
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
-// 	cleanEspresso := runEspresso()
-// 	defer cleanEspresso()
-// 	espressoStreamer := createEspressoStreamer(builder)
-// 	// Test PopMessageWithMetadataAndPos
+	builder, cleanup := createL1AndL2Node(ctx, t, true)
+	defer cleanup()
 
-// 	//Test PeekMessageWithMetadataAndPos returns nil if the queue is empty
-// 	t.Run("Test PeekMessageWithMetadataAndPos returns nil if the queue is empty", func(t *testing.T) {
-// 		// TODO: implement test
-// 		messageWithMetadataAndPos := espressoStreamer.PeekMessageWithMetadataAndPos()
-// 		if messageWithMetadataAndPos != nil {
-// 			t.Errorf("Expected nil, got %v", messageWithMetadataAndPos)
-// 		}
-// 	})
+	cleanEspresso := runEspresso()
+	defer cleanEspresso()
+	espressoStreamer := createEspressoStreamer(builder)
+	// Test PopMessageWithMetadataAndPos
 
-// 	t.Run("Test PopMessageWithMetadataAndPos returns nil if the queue is empty", func(t *testing.T) {
-// 		// TODO: implement test
-// 		messageWithMetadataAndPos := espressoStreamer.PopMessageWithMetadataAndPos()
-// 		if messageWithMetadataAndPos != nil {
-// 			t.Errorf("Expected nil, got %v", messageWithMetadataAndPos)
-// 		}
-// 	})
+	//Test PeekMessageWithMetadataAndPos returns nil if the queue is empty
+	t.Run("Test PeekMessageWithMetadataAndPos returns nil if the queue is empty", func(t *testing.T) {
+		messageWithMetadataAndPos := espressoStreamer.PeekMessageWithMetadataAndPos()
+		if messageWithMetadataAndPos != nil {
+			t.Errorf("Expected nil, got %v", messageWithMetadataAndPos)
+		}
+	})
 
-// 	t.Run("Test PeekMessageWithMetadataAndPos peeks the first message from the queue", func(t *testing.T) {
-// 		// add a new message to the queue
+	t.Run("Test PopMessageWithMetadataAndPos returns nil if the queue is empty", func(t *testing.T) {
+		// TODO: implement test
+		messageWithMetadataAndPos := espressoStreamer.PopMessageWithMetadataAndPos()
+		if messageWithMetadataAndPos != nil {
+			t.Errorf("Expected nil, got %v", messageWithMetadataAndPos)
+		}
+	})
 
-// 		// TODO: implement test
-// 	})
-// 	t.Run("Test PopMessageWithMetadataAndPos pops the first message from the queue", func(t *testing.T) {
-// 		// TODO: implement test
+	t.Run("Test PeekMessageWithMetadataAndPos peeks the first message from the queue", func(t *testing.T) {
+		// add a new message to the queue
+		// TODO: implement test
+	})
+	t.Run("Test PopMessageWithMetadataAndPos pops the first message from the queue", func(t *testing.T) {
+		// TODO: implement test
 
-// 		// create a new EspressoStreamer
+		// create a new EspressoStreamer
 
-// 	})
+	})
 
-// }
+}
