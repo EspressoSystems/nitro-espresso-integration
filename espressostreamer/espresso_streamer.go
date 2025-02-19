e69b00af00dbeb030711c5bed410fea082e78d71
Jeremy <297323986@qq.com>
2025-02-19 12:33:10 +0800

    Reset the message queue

--- espressostreamer/espresso_streamer.go
+++ espressostreamer/espresso_streamer.go
@@ -95,6 +95,7 @@ func NewEspressoStreamer(namespace uint64, hotshotUrls []string,
 func (s *EspressoStreamer) Reset(currentMessagePos uint64, currentHostshotBlock uint64) {
 	s.currentMessagePos = currentMessagePos
 	s.nextHotshotBlockNum = currentHostshotBlock
+	s.messageWithMetadataAndPos = []*MessageWithMetadataAndPos{}
 }
 
 func (s *EspressoStreamer) Next() (*MessageWithMetadataAndPos, error) {
