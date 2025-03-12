bdb53a2696c4d8fbab589a5f6694d22eb6a4349f
Zach Showalter <zacshowa@gmail.com>
2025-03-12 12:40:59 -0400

    merge multi query client

--- system_tests/espresso_sovereign_sequencer_test.go
+++ system_tests/espresso_sovereign_sequencer_test.go
@@ -38,9 +38,8 @@ func createL1AndL2Node(
 	builder.nodeConfig.BatchPoster.PollInterval = 10 * time.Second
 	builder.nodeConfig.BatchPoster.MaxDelay = -1000 * time.Hour
 	builder.nodeConfig.BatchPoster.LightClientAddress = lightClientAddress
-	builder.nodeConfig.BatchPoster.HotShotUrl = hotShotUrl
 	// Test that fallbackurl work
-	builder.nodeConfig.BatchPoster.FallBackUrl = hotShotUrl
+	builder.nodeConfig.BatchPoster.HotShotUrls = []string{hotShotUrl}
 	builder.nodeConfig.BatchPoster.UseEscapeHatch = false
 
 	// validator config
