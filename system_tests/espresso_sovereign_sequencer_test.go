2be761a6b5c6362e3b3a25c0d76fc9517f739b80
Jeremy <yiliang.he@qq.com>
2025-04-04 19:43:13 +0800

    Fund more

--- system_tests/espresso_sovereign_sequencer_test.go
+++ system_tests/espresso_sovereign_sequencer_test.go
@@ -61,7 +61,7 @@ func createL1AndL2Node(
 	mnemonic := "indoor dish desk flag debris potato excuse depart ticket judge file exit"
 	err := builder.L1Info.GenerateAccountWithMnemonic("CommitmentTask", mnemonic, 5)
 	Require(t, err)
-	builder.L1.TransferBalance(t, "Faucet", "CommitmentTask", big.NewInt(9e18), builder.L1Info)
+	builder.L1.TransferBalance(t, "Faucet", "CommitmentTask", new(big.Int).Mul(big.NewInt(9e18), big.NewInt(1000)), builder.L1Info)
 
 	return builder, cleanup
 }
