8a8976060c38f7ee907cb944efb7b79dc8e740d4
Jeremy <yiliang.he@qq.com>
2025-04-09 04:11:11 +0800

    merge VID changes

--- system_tests/caff_node_test.go
+++ system_tests/caff_node_test.go
@@ -34,10 +34,10 @@ func createCaffNode(ctx context.Context, t *testing.T, existing *NodeBuilder) (*
 	execConfig.Sequencer.CaffNodeConfig.ParentChainReader.UseFinalityData = true
 	execConfig.Sequencer.CaffNodeConfig.RecordPerformance = true
 	// for testing, we can use the same hotshot url for both
-	execConfig.Sequencer.CaffNodeConfig.HotShotUrls = []string{hotShotUrl, hotShotUrl, hotShotUrl, hotShotUrl}
-	execConfig.Sequencer.CaffNodeConfig.RetryTime = time.Second * 1
-	execConfig.Sequencer.CaffNodeConfig.HotshotPollingInterval = time.Millisecond * 100
-	nodeConfig.ParentChainReader.Enable = false
+	nodeConfig.EspressoCaffNode.HotShotUrls = []string{hotShotUrl, hotShotUrl, hotShotUrl, hotShotUrl}
+	nodeConfig.EspressoCaffNode.FallbackUrls = []string{hotShotUrl, hotShotUrl, hotShotUrl, hotShotUrl}
+	nodeConfig.EspressoCaffNode.RetryTime = time.Second * 1
+	nodeConfig.EspressoCaffNode.HotshotPollingInterval = time.Millisecond * 100
 
 	cleanup := builder.BuildEspressoCaffNode(t)
 	return builder.L2, cleanup
