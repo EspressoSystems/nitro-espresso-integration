c14aeac33205b285d6cff328291b79f64cc5dbc2
Jeremy <yiliang.he@qq.com>
2025-04-09 17:09:30 +0800

    Fix tests

--- system_tests/full_challenge_impl_test.go
+++ system_tests/full_challenge_impl_test.go
@@ -161,7 +161,7 @@ func makeBatch(t *testing.T, l2Node *arbnode.Node, l2Info *BlockchainTestInfo, b
 
 	seqNum := new(big.Int).Lsh(common.Big1, 256)
 	seqNum.Sub(seqNum, common.Big1)
-	tx, err := seqInbox.AddSequencerL2Batch99020501(sequencer, seqNum, message, big.NewInt(1), common.Address{}, big.NewInt(0), big.NewInt(0), []byte{})
+	tx, err := seqInbox.AddSequencerL2Batch99020501(sequencer, seqNum, message, big.NewInt(1), common.Address{}, big.NewInt(0), big.NewInt(0), createDummyHotShotHeightAndSignature(t))
 	Require(t, err)
 	receipt, err := EnsureTxSucceeded(ctx, backend, tx)
 	Require(t, err)
