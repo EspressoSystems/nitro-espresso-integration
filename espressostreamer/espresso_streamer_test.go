8f8a6547e7a8558f75b638c9b82bc4a9d84cca3e
Jeremy <yiliang.he@qq.com>
2025-04-09 22:58:02 +0800

    [Caff node] Cherry pick (#550)
    * [Caff Node] Clean up logs and add a performance recorder (#517) (#524)
    
    * Use existing function to convert block number to message index
    
    * Clean up logs
    
    * Add perf recorder to check the performance of the caff node
    
    * Persist hotshot number and current position
    
    * small fixes
    
    * cleanup
    
    ---------
    
    Co-authored-by: Sneh Koul <snehkoul1999@gmail.com>
    (cherry picked from commit 8e3556c6007427b36e257fa9139219f0f120c09c)
    (cherry picked from commit a9aa8533c99bba88298957595dfc63b69c0a7ec7)
    
    * [Caff node] read delayed messages from L1 (#537)
    
    * Read delayed message from L1
    
    * Update the caff node
    
    * Storage
    
    * Update espresso streamer to only validate attestation quotes
    
    * Update caff node to handle fetching delayed messages with safety tolerances
    
    * Fix off by 1 error when starting the espresso streamer for the first time and add better logging
    
    * Lint
    
    * Revert some of changes, this should pass CI as well
    
    * Reset
    
    * move delayed message fetcher to another file and add comments to code
    
    * Store the delayed count
    
    * batcher post empty delayed messages to hotshot
    
    * clean up
    
    ---------
    
    Co-authored-by: Zach Showalter <zacshowa@gmail.com>
    Co-authored-by: Sneh Koul <snehkoul1999@gmail.com>
    (cherry picked from commit 8691906ae15941fe8bf534f068e7740c5f806744)
    (cherry picked from commit 04bbff717b87da5cfe21f8e627050d40cbaa9aee)

--- espressostreamer/espresso_streamer_test.go
+++ espressostreamer/espresso_streamer_test.go
@@ -9,11 +9,13 @@ import (
 
 	espressoClient "github.com/EspressoSystems/espresso-sequencer-go/client"
 	"github.com/EspressoSystems/espresso-sequencer-go/types"
+	espressoTypes "github.com/EspressoSystems/espresso-sequencer-go/types"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/log"
 
 	"github.com/offchainlabs/nitro/arbos/arbostypes"
@@ -30,7 +32,7 @@ func TestEspressoStreamer(t *testing.T) {
 		// Simulate the call to the tee verifier returning a byte array. To the streamer, this indicates the attestation quote is valid.
 		mockEspressoTEEVerifierClient.On("Verify", mock.Anything, mock.Anything, mock.Anything).Return(nil)
 		// create a new streamer object
-		streamer := NewEspressoStreamer(1, 1, time.Millisecond, time.Millisecond, mockEspressoTEEVerifierClient, mockEspressoClient)
+		streamer := NewEspressoStreamer(1, 1, time.Millisecond, time.Millisecond, mockEspressoTEEVerifierClient, mockEspressoClient, false, common.Address{})
 		streamer.Reset(735805, 1)
 		// Get the data for this test
 		testBlocks := GetTestBlocks()
@@ -63,7 +65,7 @@ func TestEspressoStreamer(t *testing.T) {
 
 		mockEspressoClient.On("FetchTransactionsInBlock", ctx, uint64(6), namespace).Return(espressoClient.TransactionsInBlock{}, errors.New("test error"))
 
-		streamer := NewEspressoStreamer(namespace, 3, time.Millisecond, time.Millisecond, mockEspressoTEEVerifierClient, mockEspressoClient)
+		streamer := NewEspressoStreamer(namespace, 3, time.Millisecond, time.Millisecond, mockEspressoTEEVerifierClient, mockEspressoClient, false, common.Address{})
 
 		testParseFn := func(tx types.Bytes) ([]*MessageWithMetadataAndPos, error) {
 			return nil, nil
@@ -105,7 +107,7 @@ func TestEspressoStreamer(t *testing.T) {
 			},
 		}, nil)
 
-		streamer := NewEspressoStreamer(namespace, 3, time.Millisecond, time.Millisecond, mockEspressoTEEVerifierClient, mockEspressoClient)
+		streamer := NewEspressoStreamer(namespace, 3, time.Millisecond, time.Millisecond, mockEspressoTEEVerifierClient, mockEspressoClient, false, common.Address{})
 
 		testParseFn := func(pos uint64, hotshotheight uint64) func(tx types.Bytes) ([]*MessageWithMetadataAndPos, error) {
 
@@ -169,6 +171,10 @@ func (m *mockEspressoClient) FetchTransactionsInBlock(ctx context.Context, block
 	args := m.Called(ctx, blockHeight, namespace)
 	//nolint:errcheck
 	return args.Get(0).(espressoClient.TransactionsInBlock), args.Error(1)
+}
+
+func (m *mockEspressoClient) FetchHeaderByHeight(ctx context.Context, blockHeight uint64) (espressoTypes.HeaderImpl, error) {
+	panic("not implemented")
 }
 
 // To generate test scripts for the mock clients, we can create a list of test blocks that we can iterate through and set as the call and return values.
