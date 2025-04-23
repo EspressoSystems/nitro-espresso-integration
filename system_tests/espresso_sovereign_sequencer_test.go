7239df6ae723be544c69f25dabae2d26ba692e1d
Sneh Koul <snehkoul1999@gmail.com>
2025-04-24 03:38:35 +0530

    fix build

--- system_tests/espresso_sovereign_sequencer_test.go
+++ system_tests/espresso_sovereign_sequencer_test.go
@@ -7,7 +7,7 @@ import (
 	"testing"
 	"time"
 
-	lightclient "github.com/EspressoSystems/espresso-sequencer-go/light-client"
+	lightclient "github.com/EspressoSystems/espresso-network-go/light-client"
 
 	"github.com/ethereum/go-ethereum/common"
 )
