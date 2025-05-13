2586a570eaf32b736b56e7a5167f843cbf5b6dea
Luke Iannucci <luke.iannucci@outlook.com>
2025-05-13 14:47:29 -0400

    fix

--- arbnode/espresso_caff_node.go
+++ arbnode/espresso_caff_node.go
@@ -24,7 +24,6 @@ import (
 )
 
 type EspressoCaffNodeConfig struct {
-<<<<<<< HEAD
 	Enable                 bool                    `koanf:"enable"`
 	HotShotUrls            []string                `koanf:"hotshot-urls"`
 	NextHotshotBlock       uint64                  `koanf:"next-hotshot-block"`
