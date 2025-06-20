4a599d837ea896d43a6804abdf44de854c93d912
Jean Gal <jgatbkk@gmail.com>
2025-06-20 12:21:49 +0200

    Separate max fee logic from dataposter.

--- espressotee/espresso_verifier.go
+++ espressotee/espresso_verifier.go
@@ -65,6 +65,15 @@ func (e *EspressoTEEVerifier) RegisterSigner(dataPoster *dataposter.DataPoster, 
 	gasLimit := estimate * (100 + registerSignerOpts.GasLimitBufferIncreasePercent) / 100
 	log.Info("register signer gas limit", "gas limit", gasLimit)
 
+	latestBaseFee, err := dataPoster.BaseFee()
+	if err != nil {
+		return err
+	}
+
+	if latestBaseFee.Uint64() > registerSignerOpts.MaxBaseFee {
+		return fmt.Errorf("latest base fee is greater than max base fee: %d > %d", latestBaseFee.Uint64(), registerSignerOpts.MaxBaseFee)
+	}
+
 	// Since we use batch poster private key to register signer, we need to use dataposter to post transaction
 	// So the dataposter can track the proper nonce once we start posting batches
 	tx, err := dataPoster.PostSimpleTransaction(context.Background(), e.address, calldata, gasLimit, dataPoster.Auth().Value)
