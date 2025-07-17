0d743e572d590cc688891e33a653fa86e5257a7c
Sneh Koul <snehkoul1999@gmail.com>
2025-07-17 15:53:07 -0400

    fix test

--- arbnode/transaction_streamer.go
+++ arbnode/transaction_streamer.go
@@ -1920,7 +1920,7 @@ func (s *TransactionStreamer) submitEspressoTransactions(ctx context.Context) er
 		return err
 	}
 
-	if len(pendingTxnsPos) == 0 {
+	if len(pendingTxnsPos) <= 0 {
 		return nil
 	}
 	fetcher := func(pos arbutil.MessageIndex) ([]byte, error) {
