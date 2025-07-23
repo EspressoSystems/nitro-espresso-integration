786778a5402f2773a317114fdde054ece4577607
Theodore Schnepper <tschnepper@espressosys.com>
2025-07-23 07:06:11 -0600

    Split Espresso code from transaction_streamer
    In order to make the management of the Espresso integration from the rest of the
    code, it is helpful to have the Espresso additions as separate and isolated from the
    rest of the code as possible.  In this way all of the Espresso changes becomes as
    additive as possible.  This can help to make the code being added easier to review
    in isolation, only needing to inspect the bridge boundaries at the point where they
    interact.
    
    This change makes an effort to split out the Espresso submission process from the
    rest of the existing code base into its own self-contained areas.  In doing this, the
    code should be better organized, while providing an interface for the boundaries
    where these two separate systems communicate.
    
    NOTE: This can be further refined, especially around the area of configuring and
    registering the TEE
    
    (cherry picked from commit 280d41a9d962a67ddd9f6d61f7fbae8b8eb81335)

--- system_tests/espresso/transaction_streamer/mock_transaction_streamer_environment.go
+++ system_tests/espresso/transaction_streamer/mock_transaction_streamer_environment.go
@@ -10,6 +10,7 @@ import (
 	"github.com/ethereum/go-ethereum/params"
 	"github.com/offchainlabs/nitro/arbnode"
 	"github.com/offchainlabs/nitro/broadcaster"
+	"github.com/offchainlabs/nitro/espresso/submitter"
 	"github.com/offchainlabs/nitro/execution"
 	chain "github.com/offchainlabs/nitro/system_tests/espresso/chain"
 	execution_engine "github.com/offchainlabs/nitro/system_tests/espresso/execution_engine"
@@ -167,7 +168,7 @@ func WithEspressoTransactionStreamerOptions(options ...arbnode.TransactionStream
 // to the TransactionStreamer, or start producing blocks in the mock Espresso
 // Chain. Those actions are left to the caller of this function, so they are
 // able to control the timing, and configuration of those actions.
-func NewMockTransactionStreamerEnvironment(ctx context.Context, options ...MockTransactionStreamerEnvironmentOption) (*chain.MockEspressoChain, espresso_client.EspressoClient, *arbnode.TransactionStreamer, error) {
+func NewMockTransactionStreamerEnvironment(ctx context.Context, options ...MockTransactionStreamerEnvironmentOption) (*chain.MockEspressoChain, espresso_client.EspressoClient, submitter.EspressoSubmitter, *arbnode.TransactionStreamer, error) {
 	// Setup the Default Configuration for the Mock Transaction Streamer Environment
 	config := &MockTransactionStreamerEnvironmentConfig{
 		Database:     rawdb.NewMemoryDatabase(),
@@ -206,11 +207,11 @@ func NewMockTransactionStreamerEnvironment(ctx context.Context, options ...MockT
 	)
 
 	if err != nil {
-		return nil, nil, nil, ErrorFailedToCreateTransactionStreamer{Cause: err}
+		return nil, nil, nil, nil, ErrorFailedToCreateTransactionStreamer{Cause: err}
 	}
 
 	// Configure the Transaction Streamer with the Espresso Fields and options
-	arbnode.ConfigureEspressoFields(
+	submitter, err := arbnode.ConfigureEspressoFields(
 		streamer,
 		// We setup the initial values before the other options are applied,
 		// so that the other options can modify them as needed.
@@ -224,5 +225,5 @@ func NewMockTransactionStreamerEnvironment(ctx context.Context, options ...MockT
 		),
 	)
 
-	return espressoChain, espressoClient, streamer, nil
+	return espressoChain, espressoClient, submitter, streamer, err
 }
