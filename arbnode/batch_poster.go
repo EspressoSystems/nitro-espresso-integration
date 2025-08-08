755335b5169dbe49b0ce8f3b7127d79069227f37
Jeremy <yiliang.he@qq.com>
2025-08-08 20:16:40 +0800

    Backport updates to v3.5.6 (#714)
    * fix defaults (#711)
    
    (cherry picked from commit 010e4a0ea0b610f638877a9f636c3dd2c680c086)
    
    * fix comment (#712)
    
    * fix comment
    
    * add comma
    
    (cherry picked from commit 7128814d480176c1a3c272cbaab75d3d96b6e983)
    
    * Move `RegisterSigner` to `BatchPoster` (#691)
    
    * dont register signer if dataposter is less than onchain
    
    * fixes
    
    * dont send tx if dataposter is ahead of on chain
    
    * cleanup
    
    * address comments
    
    (cherry picked from commit 39eff9f47806f83cc433b7e6f062eb5b2b1aa88d)
    
    ---------
    
    Co-authored-by: Sneh Koul <35871990+Sneh1999@users.noreply.github.com>
    Co-authored-by: Luke Iannucci <luke.iannucci@outlook.com>

--- arbnode/batch_poster.go
+++ arbnode/batch_poster.go
@@ -1716,7 +1716,11 @@ func (b *BatchPoster) maybePostSequencerBatch(ctx context.Context) (bool, error)
 	if espressoSubmitter := b.streamer.espressoSubmitter; espressoSubmitter != nil {
 		registered := espressoSubmitter.GetKeyManager().HasRegistered()
 		if !registered {
-			return false, fmt.Errorf("ephemeral keys are not yet registed in Espresso TEE Contract")
+			log.Warn("ephemeral keys are not yet registered in Espresso TEE Contract")
+			err := espressoSubmitter.RegisterSigner()
+			if err != nil {
+				return false, fmt.Errorf("unable to register signer: %w", err)
+			}
 		}
 	}
 
