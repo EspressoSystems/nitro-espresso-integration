d071a1c82fcf4978d691b690b245eabd5837e530
Sneh Koul <snehkoul1999@gmail.com>
2025-08-18 12:07:06 -0400

    fix loop check

--- arbnode/batch_poster.go
+++ arbnode/batch_poster.go
@@ -2118,7 +2118,7 @@ func (b *BatchPoster) MaybePostSequencerBatch(ctx context.Context) (bool, error)
 		breakLoopWhenErrorOccurs = true
 	}
 
-	for b.building.msgCount < msgCount {
+	for b.building.msgCount <= msgCount {
 		msg, err := getNextMessage()
 		if err != nil {
 			if breakLoopWhenErrorOccurs {
