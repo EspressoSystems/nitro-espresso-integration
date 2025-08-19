ec4ba2dbd28d2017db4b277b44239d93dd61551b
Sneh Koul <35871990+Sneh1999@users.noreply.github.com>
2025-08-19 19:18:37 +0530

    Fix empty batch fix (#723)
    * Should post empty batch when empty batch delay is hit
    
    * fix loop check
    
    * fix check
    
    * add delayed message empty batch post check
    
    * revert <=
    
    * fix ci
    
    * fix lint
    
    (cherry picked from commit 30bb151676aa4bb8f9c8afc354e2a07bfd851edd)

--- arbnode/batch_poster.go
+++ arbnode/batch_poster.go
@@ -1874,6 +1874,14 @@ func (b *BatchPoster) maybePostSequencerBatch(ctx context.Context) (bool, error)
 		}
 	}
 
+	if b.building.firstDelayedMsg != nil {
+		// #nosec G115
+		timeSinceMsg := time.Since(time.Unix(int64(b.building.firstDelayedMsg.Message.Header.Timestamp), 0))
+		if timeSinceMsg >= config.MaxEmptyBatchDelay {
+			forcePostBatch = true
+		}
+	}
+
 	for b.building.msgCount < msgCount {
 		msg, err := getNextMessage()
 		if err != nil {
