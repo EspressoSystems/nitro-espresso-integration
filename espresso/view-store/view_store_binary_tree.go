bb37ddb9c8e428cfb79979bb6e21e38dcc7433d0
Sneh Koul <snehkoul1999@gmail.com>
2025-08-28 15:51:13 -0400

    add tests

--- espresso/view-store/view_store_binary_tree.go
+++ espresso/view-store/view_store_binary_tree.go
@@ -4,6 +4,7 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 )
 
+// TODO: should we store this in the database?
 type View struct {
 	viewNumber        uint64
 	builderCommitment string
@@ -13,13 +14,17 @@ type View struct {
 // ViewStoreBinaryTree is a binary tree
 // storing the state hashes of the nitro state
 // at a given view number and payload commitment
-// TODO: should we store this in the database?
 type ViewStoreBinaryTree struct {
 	view  View
 	Left  *ViewStoreBinaryTree
 	Right *ViewStoreBinaryTree
 }
 
+/*
+Insert inserts a view into the view store binary tree, on the left side of the tree all the views
+with a view number and builder commitment less than the root's view number are stored. On the right side
+all the views with a view number and builder commitment greater than the root's view number are stored.
+*/
 func Insert(root *ViewStoreBinaryTree, viewNumber uint64, builderCommitment string, stateHash common.Hash) *ViewStoreBinaryTree {
 	if root == nil {
 		return &ViewStoreBinaryTree{
@@ -32,23 +37,30 @@ func Insert(root *ViewStoreBinaryTree, viewNumber uint64, builderCommitment stri
 	}
 	if viewNumber < root.view.viewNumber {
 		root.Left = Insert(root.Left, viewNumber, builderCommitment, stateHash)
+		return root
 	} else if viewNumber > root.view.viewNumber {
 		root.Right = Insert(root.Right, viewNumber, builderCommitment, stateHash)
+		return root
 	}
 
 	// This means that view numbers is equal to the root's view number
-	// Payload commitment might be different so we insert based on that now
+	// Builder commitment might be different so we insert based on that now
 	if builderCommitment < root.view.builderCommitment {
 		root.Left = Insert(root.Left, viewNumber, builderCommitment, stateHash)
+		return root
 	} else if builderCommitment > root.view.builderCommitment {
 		root.Right = Insert(root.Right, viewNumber, builderCommitment, stateHash)
+		return root
 	}
 
-	// This means that the view numbers are equal and the payload commitment is equal
+	// This means that the view numbers are equal and the builder commitment is equal
 	return root
 }
 
-func Search(root *ViewStoreBinaryTree, viewNumber uint64, builderCommitment string) *View {
+/*
+Search searches for a view with a given view number and builder commitment
+*/
+func Search(root *ViewStoreBinaryTree, viewNumber uint64, builderCommitment string) *ViewStoreBinaryTree {
 	if root == nil {
 		return nil
 	}
@@ -58,29 +70,29 @@ func Search(root *ViewStoreBinaryTree, viewNumber uint64, builderCommitment stri
 		return Search(root.Right, viewNumber, builderCommitment)
 	}
 	if builderCommitment == root.view.builderCommitment {
-		return &root.view
+		return root
 	} else if builderCommitment < root.view.builderCommitment {
 		return Search(root.Left, viewNumber, builderCommitment)
 	} else if builderCommitment > root.view.builderCommitment {
 		return Search(root.Right, viewNumber, builderCommitment)
 	}
-	return nil
+	return root
 }
 
-func Delete(root *ViewStoreBinaryTree, viewNumber uint64) *ViewStoreBinaryTree {
+// Delete removes all nodes with viewNumber <= cutoff.
+func Delete(root *ViewStoreBinaryTree, cutoff uint64) *ViewStoreBinaryTree {
 	if root == nil {
 		return nil
 	}
-	// Find the node where this view number is located first and then delete any nodes
-	// which have the view number equal or less than the node's view number
-	if viewNumber < root.view.viewNumber {
-		root.Left = Delete(root.Left, viewNumber)
-	} else if viewNumber > root.view.viewNumber {
-		root.Right = Delete(root.Right, viewNumber)
+
+	if root.view.viewNumber > cutoff {
+		// Keep this node; only the left subtree can contain nodes to delete.
+		root.Left = Delete(root.Left, cutoff)
+		return root
 	}
 
-	if root.Left == nil && root.Right == nil {
-		return nil
-	}
-	return root
+	// root.view.viewNumber <= cutoff:
+	// Drop this node and its entire left subtree.
+	// Continue deleting in the right subtree (may contain ties == cutoff).
+	return Delete(root.Right, cutoff)
 }
