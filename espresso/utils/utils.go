d33e22a3e883797b98c3eb0801c25678db65f6a9
Sneh Koul <snehkoul1999@gmail.com>
2025-09-03 18:10:13 -0400

    fix decoding

--- espresso/utils/utils.go
+++ espresso/utils/utils.go
@@ -7,6 +7,7 @@ import (
 	"io"
 
 	"github.com/EspressoSystems/espresso-network/sdks/go/types"
+	"github.com/ethereum/go-ethereum/log"
 )
 
 const (
@@ -15,6 +16,7 @@ const (
 	OFFSET_BYTE_LEN  = 4 // offset is u32
 	ENTRY_BYTE_LEN   = NS_ID_BYTE_LEN + OFFSET_BYTE_LEN
 	LEN_SIZE         = 4
+	NUM_TXS_BYTE_LEN = 4
 )
 
 type NSRange struct {
@@ -29,22 +31,26 @@ func DecodeTransactionsPayload(encoded []byte, nsRange NSRange) ([]types.Transac
 		return nil, fmt.Errorf("encoded payload is smaller than the end of the ns range")
 	}
 	nsEncodedPayload := encoded[nsRange.Start:nsRange.End]
+
 	r := bytes.NewReader(nsEncodedPayload)
 
 	// Step 1: num_txs
-	numTxns := make([]uint8, 4)
-	if _, err := io.ReadFull(r, numTxns); err != nil {
-		return nil, fmt.Errorf("read num_txs: %w", err)
+	var numTxs uint32
+	err := binary.Read(r, binary.LittleEndian, &numTxs)
+	if err != nil {
+		return nil, fmt.Errorf("read num txs: %w", err)
 	}
-	numTxs := binary.LittleEndian.Uint32(numTxns)
 
 	// Step 2: offsets
 	offsets := make([]uint32, numTxs)
 	for i := range offsets {
 		if err := binary.Read(r, binary.LittleEndian, &offsets[i]); err != nil {
-			return nil, fmt.Errorf("read offset[%d]: %w", i, err)
+			return nil, fmt.Errorf("read offsets: %w", err)
 		}
 	}
+
+	log.Info("num txs of decode transactions", "numTxs", numTxs)
+	log.Info("offsets of decode transactions", "offsets", offsets)
 
 	// Step 3: tx_bodies = remaining bytes
 	txBodies, err := io.ReadAll(r)
@@ -56,15 +62,18 @@ func DecodeTransactionsPayload(encoded []byte, nsRange NSRange) ([]types.Transac
 	var txs []types.Transaction
 	start := uint32(0)
 	for i := 0; i < int(numTxs); i++ {
-		end := offsets[i]
+		end := start + offsets[i]
 		txBytes := txBodies[start:end]
-
+		log.Info("start of decode transactions", "start", start)
+		log.Info("end of decode transactions", "end", end)
+		log.Info("txBytes of decode transactions", "txBytes", txBytes)
 		txs = append(txs, types.Transaction{
 			Payload: txBytes,
 		})
 		start = end
 	}
 
+	log.Info("transaction after decoding", "txs", txs)
 	return txs, nil
 }
 
@@ -85,15 +94,25 @@ func DecodeNSTable(encoded []byte, namespaceId uint32) (*NSRange, error) {
 		offset := binary.LittleEndian.Uint32(encoded[off : off+OFFSET_BYTE_LEN])
 		off += OFFSET_BYTE_LEN
 
+		end := start + offset
+
 		if namespaceId == nsId {
+			log.Info("nsId of ns table", "nsId", nsId)
+			log.Info("offset of ns table", "offset", offset)
+			log.Info("end of ns table", "end", end)
+			log.Info("start of ns table", "start", start)
 			return &NSRange{
 				NsId:  namespaceId,
-				End:   offset,
+				End:   end,
 				Start: start,
 			}, nil
 		}
-		start = offset
+		start = end
 	}
 
 	return nil, fmt.Errorf("ns table not found")
 }
+
+// 0...250
+// 250..350
+//
