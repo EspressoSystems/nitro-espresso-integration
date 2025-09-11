53d5c87fefc8dc90156b932ac1bcf9d846f30bb1
Sneh Koul <snehkoul1999@gmail.com>
2025-09-11 15:01:21 -0400

    fix test

--- system_tests/bold_challenge_protocol_test.go
+++ system_tests/bold_challenge_protocol_test.go
@@ -637,6 +637,7 @@ func createTestNodeOnL1ForBoldProtocol(
 		addresses, sequencerTxOptsPtr, sequencerTxOptsPtr, dataSigner, fatalErrChan, parentChainId,
 		nil, // Blob reader.
 		locator.LatestWasmModuleRoot(),
+		nil,
 	)
 	Require(t, err)
 
@@ -845,7 +846,7 @@ func create2ndNodeWithConfigForBoldProtocol(
 	Require(t, err)
 	locator, err := server_common.NewMachineLocator("")
 	Require(t, err)
-	l2node, err := arbnode.CreateNodeFullExecutionClient(ctx, l2stack, execNode, execNode, execNode, execNode, l2arbDb, NewFetcherFromConfig(nodeConfig), l2blockchain.Config(), l1client, addresses, &txOpts, &txOpts, dataSigner, fatalErrChan, l1ChainId, nil /* blob reader */, locator.LatestWasmModuleRoot())
+	l2node, err := arbnode.CreateNodeFullExecutionClient(ctx, l2stack, execNode, execNode, execNode, execNode, l2arbDb, NewFetcherFromConfig(nodeConfig), l2blockchain.Config(), l1client, addresses, &txOpts, &txOpts, dataSigner, fatalErrChan, l1ChainId, nil /* blob reader */, locator.LatestWasmModuleRoot(), nil)
 	Require(t, err)
 
 	l2client := ClientForStack(t, l2stack)
