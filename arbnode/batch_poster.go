465e8683605e8373db459be0f923f401ab94eb45
Jeremy <yiliang.he@qq.com>
2025-09-22 10:44:26 +0800

    Fix empty batch

--- arbnode/batch_poster.go
+++ arbnode/batch_poster.go
@@ -1890,6 +1890,15 @@ func (b *BatchPoster) maybePostSequencerBatch(ctx context.Context) (bool, error)
 		}
 	}
 
+	if b.building.firstDelayedMsg != nil && b.espressoStreamer != nil {
+		// #nosec G115
+		timeSinceMsg := time.Since(time.Unix(int64(b.building.firstDelayedMsg.Message.Header.Timestamp), 0))
+		if timeSinceMsg >= config.MaxEmptyBatchDelay {
+			forcePostBatch = true
+			b.building.haveUsefulMessage = true
+		}
+	}
+
 	for b.building.msgCount < msgCount {
 		msg, err := getNextMessage()
 		if err != nil {
