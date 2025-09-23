9429d21837fafc67b666bcf87ceeb42cf87ee4ef
Zach Showalter <zacshowa@gmail.com>
2025-09-23 18:52:54 -0400

    Attempt to use new bindings in batch poster

--- arbnode/batch_poster.go
+++ arbnode/batch_poster.go
@@ -19,6 +19,7 @@ import (
 	hotshotClient "github.com/EspressoSystems/espresso-network/sdks/go/client"
 	lightclient "github.com/EspressoSystems/espresso-network/sdks/go/light-client"
 	"github.com/andybalholm/brotli"
+	"github.com/espressosystems/espresso-tee-contracts/espressogen"
 	"github.com/spf13/pflag"
 
 	"github.com/ethereum/go-ethereum"
@@ -51,7 +52,6 @@ import (
 	"github.com/offchainlabs/nitro/espressostreamer"
 	"github.com/offchainlabs/nitro/espressotee"
 	"github.com/offchainlabs/nitro/execution"
-	"github.com/offchainlabs/nitro/solgen/go/espressogen"
 	"github.com/offchainlabs/nitro/util"
 	"github.com/offchainlabs/nitro/util/arbmath"
 	"github.com/offchainlabs/nitro/util/blobs"
