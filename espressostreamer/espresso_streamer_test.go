f16f52d48fbefdb30489e07e212d77966bd41ca4
Zach Showalter <zacshowa@gmail.com>
2025-10-02 23:35:47 -0400

    Update  with new bindings.

--- espressostreamer/espresso_streamer_test.go
+++ espressostreamer/espresso_streamer_test.go
@@ -23,6 +23,7 @@ import (
 
 	"github.com/offchainlabs/nitro/arbos/arbostypes"
 	"github.com/offchainlabs/nitro/arbutil"
+	"github.com/offchainlabs/nitro/espressotee"
 	"github.com/offchainlabs/nitro/solgen/go/espressogen"
 )
 
@@ -304,7 +305,7 @@ type mockEspressoTEEVerifier struct {
 	mock.Mock
 }
 
-func (v *mockEspressoTEEVerifier) Verify(opts *bind.CallOpts, attestation []byte, signature [32]byte) (espressogen.EnclaveReport, error) {
+func (v *mockEspressoTEEVerifier) Verify(opts *bind.CallOpts, attestation []byte, signature [32]byte, serviceType espressotee.ServiceType) (espressogen.EnclaveReport, error) {
 	return espressogen.EnclaveReport{}, nil
 }
 
