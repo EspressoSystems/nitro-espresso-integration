94ba9ac36df6c62d4083d42765cad730c4fa8ba6
Alex Xiong <alex.xiong.tech@gmail.com>
2025-10-15 17:02:50 -0400

    refactor auth r/w for block, header, receipts

--- espresso/authdb/accessors_chain.go
+++ espresso/authdb/accessors_chain.go
@@ -2,6 +2,7 @@ package authdb
 
 import (
 	"crypto/hmac"
+	"fmt"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
@@ -9,102 +10,127 @@ import (
 	"github.com/ethereum/go-ethereum/rlp"
 )
 
-func (d *AuthDB) readHeader(hash common.Hash, number uint64) *types.Header {
-	headerKey := _headerKey(number, hash)
-	// Get the header bytes
+func (d *AuthDB) authReadHeader(hash common.Hash, number uint64) ([]byte, error) {
+	headerKey := headerKey(number, hash)
 	headerBytes, err := d.db.Get(headerKey)
 	if err != nil {
 		log.Error("Failed to get header bytes", "hash", hash, "err", err)
-		return nil
+		return nil, err
 	}
 
-	// Contract block from headerBytes
 	header := new(types.Header)
 	err = rlp.DecodeBytes(headerBytes, header)
 	if err != nil {
 		log.Error("Failed to decode header", "hash", hash, "err", err)
-		return nil
+		return nil, err
+	}
+
+	expectedTag, err := d.db.Get(headerAuthTagKey(number, hash))
+	if err != nil {
+		log.Error("Failed to get header auth tag", "hash", hash, "err", err)
+		return nil, err
 	}
 
 	d.mac.Write(headerKey)
 	d.mac.Write(headerBytes)
-	taggedBlockHash := d.mac.Sum(nil)
+	tag := d.mac.Sum(nil)
 	d.mac.Reset()
 
-	if !hmac.Equal(taggedBlockHash, header.Hash().Bytes()) {
-		log.Error("Block hash mismatch", "hash", hash, "number", number)
-		return nil
+	if !hmac.Equal(tag, expectedTag) {
+		log.Error("failed to authenticate header", "hash", hash, "number", number)
+		return nil, fmt.Errorf("failed to authenticate header, hash: %v, number: %d", hash, number)
 	}
 
-	if header.Number.Uint64() != number {
-		log.Error("Block number mismatch", "hash", hash, "number", number)
-		return nil
-	}
-
-	return header
+	return headerBytes, nil
 }
 
-func (d *AuthDB) readBody(hash common.Hash, number uint64) *types.Body {
-	block := d.readBlock(hash, number)
-	if block == nil {
-		return nil
-	}
-
-	return block.Body()
-}
-
-func (d *AuthDB) readReceipts(hash common.Hash, number uint64) *types.Receipts {
-	block := d.readBlock(hash, number)
-	if block == nil {
-		return nil
-	}
-	// TODO
-	return nil
-}
-
-func (d *AuthDB) readBlock(hash common.Hash, number uint64) *types.Block {
-	blockKey := blockKey(number, hash)
-	// Get the block bytes
-	blockBytes, err := d.db.Get(blockKey)
+func (d *AuthDB) authReadBody(hash common.Hash, number uint64) ([]byte, error) {
+	bodyKey := blockBodyKey(number, hash)
+	bodyBytes, err := d.db.Get(bodyKey)
 	if err != nil {
-		log.Error("Failed to get block bytes", "number", number, "hash", hash, "err", err)
-		return nil
+		log.Error("Failed to get body bytes", "hash", hash, "err", err)
+		return nil, err
 	}
 
-	if len(blockBytes) == 0 {
-		log.Warn("Empty block bytes", "number", number, "hash", hash)
-		return nil
-	}
-
-	// Contract block from blockBytes
-	block := new(types.Block)
-	err = rlp.DecodeBytes(blockBytes, block)
+	body := new(types.Body)
+	err = rlp.DecodeBytes(bodyBytes, body)
 	if err != nil {
-		log.Error("Failed to decode block", "number", number, "hash", hash, "err", err)
-		return nil
+		log.Error("Failed to decode body", "hash", hash, "err", err)
+		return nil, err
 	}
 
-	d.mac.Write(blockKey)
-	d.mac.Write(blockBytes)
-	taggedBlockHash := d.mac.Sum(nil)
+	expectedTag, err := d.db.Get(bodyAuthTagKey(number, hash))
+	if err != nil {
+		log.Error("Failed to get body auth tag", "hash", hash, "err", err)
+		return nil, err
+	}
+
+	d.mac.Write(bodyKey)
+	d.mac.Write(bodyBytes)
+	tag := d.mac.Sum(nil)
 	d.mac.Reset()
 
-	if !hmac.Equal(taggedBlockHash, block.Hash().Bytes()) {
-		log.Error("Block hash mismatch", "number", number, "hash", hash, "blockHash", hash)
-		return nil
+	if !hmac.Equal(tag, expectedTag) {
+		log.Error("failed to authenticate body", "hash", hash, "number", number)
+		return nil, fmt.Errorf("failed to authenticate body, hash: %v, number: %d", hash, number)
 	}
 
-	if block.Header().Number.Uint64() != number {
-		log.Error("Block number mismatch", "number", number, "hash", hash, "number", number)
-		return nil
-	}
-	return block
+	return bodyBytes, nil
 }
 
-func (d *AuthDB) readHeaderHash(hash common.Hash, number uint64) common.Hash {
-	header := d.readHeader(hash, number)
-	if header == nil {
-		return common.Hash{}
+func (d *AuthDB) authReadReceipts(hash common.Hash, number uint64) ([]byte, error) {
+	receiptsKey := blockBodyKey(number, hash)
+	receiptsBytes, err := d.db.Get(receiptsKey)
+	if err != nil {
+		log.Error("Failed to get receipts bytes", "hash", hash, "err", err)
+		return nil, err
 	}
-	return header.Hash()
+
+	receipts := new(types.Receipts)
+	err = rlp.DecodeBytes(receiptsBytes, receipts)
+	if err != nil {
+		log.Error("Failed to decode receipts", "hash", hash, "err", err)
+		return nil, err
+	}
+
+	expectedTag, err := d.db.Get(receiptsAuthTagKey(number, hash))
+	if err != nil {
+		log.Error("Failed to get receipts auth tag", "hash", hash, "err", err)
+		return nil, err
+	}
+
+	d.mac.Write(receiptsKey)
+	d.mac.Write(receiptsBytes)
+	tag := d.mac.Sum(nil)
+	d.mac.Reset()
+
+	if !hmac.Equal(tag, expectedTag) {
+		log.Error("failed to authenticate receipts", "hash", hash, "number", number)
+		return nil, fmt.Errorf("failed to authenticate receipts, hash: %v, number: %d", hash, number)
+	}
+
+	return receiptsBytes, nil
 }
+
+// func (d *AuthDB) authReadBlock(hash common.Hash, number uint64) ([]byte, error) {
+// 	bodyBytes, err := d.authReadBody(hash, number)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	body := new(types.Body)
+// 	if err := rlp.DecodeBytes(bodyBytes, body); err != nil {
+// 		return nil, fmt.Errorf("failed to decode body: %w", err)
+// 	}
+
+// 	headerBytes, err := d.authReadHeader(hash, number)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	header := new(types.Header)
+// 	if err := rlp.DecodeBytes(headerBytes, header); err != nil {
+// 		return nil, fmt.Errorf("failed to decode header: %w", err)
+// 	}
+
+// 	block := types.NewBlockWithHeader(header).WithBody(*body)
+// 	return rlp., nil
+// }
