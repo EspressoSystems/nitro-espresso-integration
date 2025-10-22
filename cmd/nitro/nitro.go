74a81e5d13d2cabe7d3759a7011b7a871da3ae72
Zach Showalter <zacshowa@gmail.com>
2025-10-22 11:41:59 -0500

    Update private key export env var

--- cmd/nitro/nitro.go
+++ cmd/nitro/nitro.go
@@ -272,7 +272,7 @@ func mainImpl() int {
 		}
 		privHex := hex.EncodeToString(key.D.Bytes())
 		// This will be used by the hyperlane validator
-		os.Setenv("SNAPSHOT_PRIVATE_KEY", privHex)
+		os.Setenv("VALIDATOR_KEY", privHex)
 		if nodeConfig.ParentChain.ID != 0 {
 			caffNodetxOpts, err = bind.NewKeyedTransactorWithChainID(key, new(big.Int).SetUint64(nodeConfig.ParentChain.ID))
 			if err != nil {
