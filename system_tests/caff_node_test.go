8bd0d7315b367631fd1fc45356b58557df55be8d
Zach Showalter <zacshowa@gmail.com>
2025-10-22 10:17:07 -0500

    Fix `TestEspressoCaffNodeRestartWithTeeType`
    This commit fixes a caff node test that was intended to have a AuthDB
    initialized with an HMAC value from the start. A previous commit
    inadvertently changed this to have an AuthDB that didn't perform HMAC
    verificaiton on it's first run, which caused it to fail on restart.

--- system_tests/caff_node_test.go
+++ system_tests/caff_node_test.go
@@ -582,7 +582,7 @@ func TestEspressoCaffNodeRestartWithTeeType(t *testing.T) {
 	// start the node
 	log.Info("Starting the caff node initially")
 	// Start the caff node without a snapshot signer
-	builderCaffNode, _, err := createCaffNode(ctx, t, builder, false, false)
+	builderCaffNode, _, err := createCaffNode(ctx, t, builder, false, true)
 	Require(t, err)
 
 	err = checkTransferTxOnL2(t, ctx, builder.L2, "User14", builder.L2Info)
