b9ecf25fbaebe0c99455493f8cf6a2592193cf33
Jeremy <yiliang.he@qq.com>
2025-11-10 11:40:07 +0800

    Improve logs (#825)

--- arbnode/batch_poster.go
+++ arbnode/batch_poster.go
@@ -1932,6 +1932,7 @@ func (b *BatchPoster) MaybePostSequencerBatch(ctx context.Context) (bool, error)
 			if batchPosition.HotShotBlockNumber > 0 {
 				b.espressoStreamer.Reset(uint64(batchPosition.MessageCount), uint64(batchPosition.HotShotBlockNumber))
 			} else {
+				log.Info("resetting streamer to parent chain", "messageCount", batchPosition.MessageCount)
 				// Fallback. For existing queued batches, we don't have the hotshot block number, so we reset to the parent chain.
 				b.resetStreamerToParentChainOrConfigHotshotBlock(batchPosition.MessageCount, ctx)
 			}
@@ -1950,6 +1951,7 @@ func (b *BatchPoster) MaybePostSequencerBatch(ctx context.Context) (bool, error)
 					if err != nil {
 						return false, err
 					}
+					log.Info("submitted pending transactions after restart", "from", batchPosition.MessageCount, "count", len(queue))
 				}
 				b.espressoRestarting = false
 			}
@@ -2144,6 +2146,11 @@ func (b *BatchPoster) MaybePostSequencerBatch(ctx context.Context) (bool, error)
 			forcePostBatch = true
 			b.building.haveUsefulMessage = true
 		}
+		log.Info("reach max empty batch delay",
+			"firstDelayedMsgTimestamp", b.building.firstDelayedMsg.Message.Header.Timestamp,
+			"timeSinceMsg", timeSinceMsg,
+			"maxEmptyBatchDelay", config.MaxEmptyBatchDelay,
+		)
 	}
 
 	for b.building.msgCount < msgCount {
