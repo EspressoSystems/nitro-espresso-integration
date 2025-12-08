17f7aa41e37e2b9aa744728c6ff40d97668c239d
Jeremy <yiliang.he@qq.com>
2025-12-08 18:01:07 +0800

    Fix caff node

--- system_tests/caff_node_test.go
+++ system_tests/caff_node_test.go
@@ -240,7 +240,7 @@ func TestEspressoCaffNode(t *testing.T) {
 	// don't make the caff node wait for finalization during the default test.
 	builder.nodeConfig.EspressoCaffNode.WaitForFinalization = false
 	// start the node
-	builder, _, err = createCaffNode(ctx, t, builder, arbnode.TestBatchPosterConfig.DisableDapFallbackStoreDataOnChain)
+	builder, _, err = createCaffNode(ctx, t, builder, false)
 	Require(t, err)
 	builderCaffNode := builder.L2
 
