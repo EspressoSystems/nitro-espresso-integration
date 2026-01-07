1c267740e5a764e5026b2ef429a87f5664f01033
Sneh Koul <snehkoul1999@gmail.com>
2026-01-07 15:25:45 -0500

    fix lint

--- arbnode/batch_poster.go
+++ arbnode/batch_poster.go
@@ -583,11 +583,6 @@ func NewBatchPoster(ctx context.Context, opts *BatchPosterOpts) (*BatchPoster, e
 		if hotShotUrl != "" {
 			hotShotClient := hotshotClient.NewClient(hotShotUrl)
 			submitterOptions = append(submitterOptions, submitter.WithEspressoClient(hotShotClient))
-
-			if err != nil {
-				return nil, fmt.Errorf("failed to create espresso original submitter: %w", err)
-			}
-
 			// If hotshot url is set, also set the sequencer inbox
 			if seqInbox == nil {
 				log.Error("espresso mode enabled without a sequencer inbox address")
