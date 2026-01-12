0b547c2dc9b5e7f98c8a447ec75ccb0684859588
Varun Doshi <doshivarun202@gmail.com>
2026-01-12 17:30:31 +0530

    rm light client reader from polling esp streamer (#907)

--- espresso/submitter/polling_espresso_submitter.go
+++ espresso/submitter/polling_espresso_submitter.go
@@ -9,7 +9,6 @@ import (
 	"time"
 
 	espresso_client "github.com/EspressoSystems/espresso-network/sdks/go/client"
-	espresso_light_client "github.com/EspressoSystems/espresso-network/sdks/go/light-client"
 	tagged_base64 "github.com/EspressoSystems/espresso-network/sdks/go/tagged-base64"
 	espresso_types "github.com/EspressoSystems/espresso-network/sdks/go/types"
 	"github.com/ccoveille/go-safecast"
@@ -57,7 +56,6 @@ type PollingEspressoSubmitter struct {
 	db                 ethdb.Database
 	messageGetter      MessageGetter
 	espressoClient     espresso_client.EspressoClient
-	lightClientReader  espresso_light_client.LightClientReaderInterface
 	espressoKeyManager espresso_key_manager.EspressoKeyManagerInterface
 
 	canSubmit func(ctx context.Context) (bool, error)
@@ -93,7 +91,6 @@ func NewPollingEspressoSubmitter(options ...EspressoSubmitterConfigOption) (Espr
 		db:                 config.Db,
 		messageGetter:      config.MessageGetter,
 		espressoClient:     config.EspressoClient,
-		lightClientReader:  config.LightClientReader,
 		espressoKeyManager: config.KeyManager,
 
 		chainID:                          config.ChainID,
@@ -536,7 +533,7 @@ func (s *PollingEspressoSubmitter) pollToResubmitEspressoTransactions(ctx contex
 // are not met, we will not submit the transaction to Espresso.
 //
 // The necessary conditions are:
-//   - The Espresso Client and Light Client Reader must be set
+//   - The Espresso Client must be set
 //   - The given `pos` parameter must be after our recorded finalized sequencer
 //     message count
 //
@@ -610,7 +607,7 @@ func (s *PollingEspressoSubmitter) Start(sw *stopwaiter.StopWaiter) error {
 			return err
 		}
 	} else {
-		log.Warn("light client reader or espresso client not set, skipping espresso verification")
+		log.Warn("espresso client not set, skipping espresso verification")
 	}
 
 	return nil
