e222c3e90054f0624c79e5dbb8561f5d89ede991
Jeremy <yiliang.he@qq.com>
2026-01-19 15:30:08 +0800

    Fix throughput test

--- arbnode/espresso_transaction_streamer_helpers.go
+++ arbnode/espresso_transaction_streamer_helpers.go
@@ -29,10 +29,8 @@ type TransactionStreamerEspressoConfig struct {
 	TxnsSendingInterval                   time.Duration
 	TxnsResubmissionInterval              time.Duration
 	ResubmitEspressoTxDeadline            time.Duration
-	UseEscapeHatch                        bool
 	EscapeHatchEnabled                    bool
 	MaxTransactionSize                    int64
-	MaxBlockLagBeforeEscapeHatch          uint64
 	InitialFinalizedSequencerMessageCount *big.Int
 
 	SubmitterCreator       func(options ...submitter.EspressoSubmitterConfigOption) (submitter.EspressoSubmitter, error)
@@ -99,14 +97,6 @@ func WithResubmitEspressoTxDeadline(deadline time.Duration) TransactionStreamerE
 	}
 }
 
-// WithUseEscapeHatch is a functional option to enable or disable the use of the
-// escape hatch in the TransactionStreamerEspressoConfig.
-func WithUseEscapeHatch(enable bool) TransactionStreamerEspressoOption {
-	return func(config *TransactionStreamerEspressoConfig) {
-		config.UseEscapeHatch = enable
-	}
-}
-
 // WithMaxTransactionSize is a functional option to set the maximum transaction
 // size in the TransactionStreamerEspressoConfig.
 func WithEscapeHatchEnabled(enable bool) TransactionStreamerEspressoOption {
@@ -121,15 +111,6 @@ func WithEscapeHatchEnabled(enable bool) TransactionStreamerEspressoOption {
 func WithMaxTransactionSize(size int64) TransactionStreamerEspressoOption {
 	return func(config *TransactionStreamerEspressoConfig) {
 		config.MaxTransactionSize = size
-	}
-}
-
-// WithMaxBlockLagBeforeEscapeHatch is a functional option to set the maximum
-// block lag before the escape hatch is triggered in the
-// TransactionStreamerEspressoConfig.
-func WithMaxBlockLagBeforeEscapeHatch(maxBlockLag uint64) TransactionStreamerEspressoOption {
-	return func(config *TransactionStreamerEspressoConfig) {
-		config.MaxBlockLagBeforeEscapeHatch = maxBlockLag
 	}
 }
 
