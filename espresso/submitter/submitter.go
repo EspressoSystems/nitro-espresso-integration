f4f40c06936241dc82b02c50921f334f51149c64
Luke Iannucci <luke.iannucci@outlook.com>
2026-02-04 15:24:12 -0500

    set check to max at 2 seconds

--- espresso/submitter/submitter.go
+++ espresso/submitter/submitter.go
@@ -396,8 +396,8 @@ func ValidateEspressoSubmitterConfig(config EspressoSubmitterConfig) error {
 		return fmt.Errorf("espresso transactions polling interval must be greater than 0")
 	}
 
-	if config.EspressoTxnsMonitoringInterval >= 10*time.Second {
-		return fmt.Errorf("espresso transactions polling interval must be 10 seconds or less")
+	if config.EspressoTxnsMonitoringInterval >= 2*time.Second {
+		return fmt.Errorf("espresso transactions polling interval must be 2 seconds or less")
 	}
 
 	if config.EspressoTxnSendingInterval <= 0 {
