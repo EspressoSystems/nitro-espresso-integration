bbb6b94b4bfd76c668172431c890fb8078676dbb
Luke Iannucci <luke.iannucci@outlook.com>
2026-04-05 20:55:38 -0400

    fix tests

--- espresso/key-manager/key_manager_test.go
+++ espresso/key-manager/key_manager_test.go
@@ -103,37 +103,26 @@ func TestEspressoKeyManager(t *testing.T) {
 		mockEspressoTEEVerifierClient.On("RegisteredServices", mock.Anything, mock.Anything, mock.Anything).Return(false, nil).Once()
 		mockEspressoTEEVerifierClient.On("RegisteredServices", mock.Anything, mock.Anything, mock.Anything).Return(true, nil).Once()
 		mockEspressoTEEVerifierClient.On("CheckNonceValidation", mock.Anything).Return(nil)
-		km := espresso_key_manager.NewEspressoKeyManager(mockEspressoTEEVerifierClient, dataposter, dataSigner, espresso_key_manager.SGX, espressotee.Test, persistentPrivKey, "", "", 0)
-		state := km.GetKeyManagerState()
-		assert.Equal(t, state, espresso_key_manager.Init, "Should start unregistered")
+		km := espresso_key_manager.NewEspressoKeyManager(mockEspressoTEEVerifierClient, dataposter, dataSigner, espresso_key_manager.TESTS, espressotee.Test, persistentPrivKey, "", "", 0)
+		assert.Equal(t, espresso_key_manager.Init, km.GetKeyManagerState(), "Should start unregistered")
 
-		// Mock sign function
-		called := false
-		getAttestationFunc := func(data []byte) ([]byte, error) {
-			called = true
-			addr := crypto.PubkeyToAddress(*km.GetCurrentKey())
-			addrBytes := addr.Bytes()
-			assert.Equal(t, addrBytes, data, "Sign function should receive public key")
-			return []byte("mock-signature"), nil
-		}
+		// Init → PendingDataPosterSync
+		registered, err := km.CheckRegistration()
+		require.NoError(t, err)
+		assert.False(t, registered)
+		assert.Equal(t, espresso_key_manager.PendingDataPosterSync, km.GetKeyManagerState())
 
-		// Register: should call sign function
-		err := km.InitRegistration(getAttestationFunc)
-		require.NoError(t, err, "Registry should succeed")
-		assert.True(t, called, "Sign function should be called")
-		state = km.GetKeyManagerState()
-		assert.Equal(t, state, espresso_key_manager.PendingDataPosterSync, "Should be pending data poster sync after call")
+		// PendingDataPosterSync → PendingRegistration
+		registered, err = km.CheckRegistration()
+		require.NoError(t, err)
+		assert.False(t, registered)
+		assert.Equal(t, espresso_key_manager.PendingRegistration, km.GetKeyManagerState())
 
-		// Second call (already registered)
-		err = km.PendingDataPosterSync()
-		require.NoError(t, err, "Registry should succeed")
-		state = km.GetKeyManagerState()
-		assert.Equal(t, state, espresso_key_manager.PendingRegistration, "Should be pending registration after call")
-
-		err = km.RegisterService()
-		require.NoError(t, err, "Registry should succeed")
-		state = km.GetKeyManagerState()
-		assert.Equal(t, state, espresso_key_manager.Registered, "Should be registered after call")
+		// PendingRegistration → Registered
+		registered, err = km.CheckRegistration()
+		require.NoError(t, err)
+		assert.True(t, registered)
+		assert.Equal(t, espresso_key_manager.Registered, km.GetKeyManagerState())
 	})
 
 	// Test GetCurrentKey
@@ -199,35 +188,25 @@ func TestEspressoKeyManager(t *testing.T) {
 		mockEspressoTEEVerifierClient.On("RegisteredServices", mock.Anything, mock.Anything, mock.Anything).Return(true, nil).Once()
 		mockEspressoTEEVerifierClient.On("CheckNonceValidation", mock.Anything).Return(nil)
 		km := espresso_key_manager.NewEspressoKeyManager(mockEspressoTEEVerifierClient, dataposter, dataSigner, espresso_key_manager.TESTS, espressotee.Test, nil, "", "", 0)
-		state := km.GetKeyManagerState()
-		assert.Equal(t, state, espresso_key_manager.Init, "Should start unregistered")
+		assert.Equal(t, espresso_key_manager.Init, km.GetKeyManagerState(), "Should start unregistered")
 
-		// Mock sign function
-		called := false
-		getAttestationFunc := func(data []byte) ([]byte, error) {
-			called = true
-			pubKeyBytes := crypto.FromECDSAPub(km.GetCurrentKey())
-			assert.Equal(t, pubKeyBytes, data, "Sign function should receive public key")
-			return []byte{}, nil
-		}
+		// Init → PendingDataPosterSync
+		registered, err := km.CheckRegistration()
+		require.NoError(t, err)
+		assert.False(t, registered)
+		assert.Equal(t, espresso_key_manager.PendingDataPosterSync, km.GetKeyManagerState())
 
-		// Register: should call sign function
-		err := km.InitRegistration(getAttestationFunc)
-		require.NoError(t, err, "Registry should succeed")
-		assert.True(t, called, "Sign function should be called")
-		state = km.GetKeyManagerState()
-		assert.Equal(t, state, espresso_key_manager.PendingDataPosterSync, "Should be pending data poster sync after call")
+		// PendingDataPosterSync → PendingRegistration
+		registered, err = km.CheckRegistration()
+		require.NoError(t, err)
+		assert.False(t, registered)
+		assert.Equal(t, espresso_key_manager.PendingRegistration, km.GetKeyManagerState())
 
-		// Second call (already registered)
-		err = km.PendingDataPosterSync()
-		require.NoError(t, err, "Registry should succeed")
-		state = km.GetKeyManagerState()
-		assert.Equal(t, state, espresso_key_manager.PendingRegistration, "Should be pending registration after call")
-
-		err = km.RegisterService()
-		require.NoError(t, err, "Registry should succeed")
-		state = km.GetKeyManagerState()
-		assert.Equal(t, state, espresso_key_manager.Registered, "Should be registered after call")
+		// PendingRegistration → Registered
+		registered, err = km.CheckRegistration()
+		require.NoError(t, err)
+		assert.True(t, registered)
+		assert.Equal(t, espresso_key_manager.Registered, km.GetKeyManagerState())
 	})
 
 	// Test Sign
